14fbf0ddfa490fd232f7f1a33784cf5adbbb6461
sERGIO <serg.ribmat@gmail.com>
2021-03-22 19:27:14 +0100

    add profile fragment

--- app/src/main/java/com/example/android/navdrawertest/ui/slideshow/ProfileFragment.kt
+++ app/src/main/java/com/example/android/navdrawertest/ui/slideshow/ProfileFragment.kt
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.navdrawertest.R
 
-class SlideshowFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
-    private lateinit var slideshowViewModel: SlideshowViewModel
+    private lateinit var profileViewModel: ProfileViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        slideshowViewModel =
-                ViewModelProvider(this).get(SlideshowViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+        profileViewModel =
+                ViewModelProvider(this).get(ProfileViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_profile, container, false)
         val textView: TextView = root.findViewById(R.id.text_slideshow)
-        slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
+        profileViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
